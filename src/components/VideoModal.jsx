3a2af9b6c1b769c74653563f9c4bf9ea0647ee9b
laura <lauracampagonzalez@gmail.com>
2025-10-01 10:19:56 +0200

    problema re-render 2

--- src/components/VideoModal.jsx
+++ src/components/VideoModal.jsx
@@ -1,37 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
 
 export default function VideoModal() {
   const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <div className="text-center">
-      {/* Botón para abrir el modal */}
-      <button
-        onClick={() => setIsOpen(true)}
-        className="text-sky-600 font-semibold hover:underline tracking-wide">
-        Ver video
-      </button>
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
 
-      {/* Modal */}
-      {isOpen && (
-        <div className="absolute inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50">
-          <div className="bg-white p-4 rounded-lg shadow-lg max-w-3xl w-full relative">
-            {/* Botón de cerrar */}
-            <button
-              onClick={() => setIsOpen(false)}
-              className="z-50 absolute top-2 right-2 text-black hover:text-gray-700 text-4xl font-bold bg-gray-200 rounded-full w-12 h-12 flex items-center justify-center cursor-pointer"
-              aria-label="Cerrar">
-              &times;
-            </button>
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
 
-            {/* Video */}
-            <video controls autoPlay muted className="w-full rounded-md ">
-              <source src="/videos/GestorDeCitas.mp4" type="video/mp4" />
-              Este navegador no soporta la reproducción de video.
-            </video>
-          </div>
-        </div>
-      )}
+  const modalContent = (
+    <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50">
+      <div className="bg-white p-4 rounded-lg shadow-lg max-w-3xl w-full relative">
+        <button
+          onClick={() => setIsOpen(false)}
+          className="z-50 absolute top-2 right-2 text-black hover:text-gray-700 text-4xl font-bold bg-gray-200 rounded-full w-12 h-12 flex items-center justify-center cursor-pointer"
+          aria-label="Cerrar">
+          &times;
+        </button>
+
+        <video controls autoPlay muted className="w-full rounded-md">
+          <source src="/videos/GestorDeCitas.mp4" type="video/mp4" />
+          Tu navegador no soporta la reproducción de video.
+        </video>
+      </div>
     </div>
+  );
+
+  return (
+    <>
+      <div className="text-center">
+        <button
+          onClick={() => setIsOpen(true)}
+          className="text-sky-600 font-semibold hover:underline tracking-wide">
+          Ver video
+        </button>
+      </div>
+
+      {isOpen && createPortal(modalContent, document.body)}
+    </>
   );
 }
